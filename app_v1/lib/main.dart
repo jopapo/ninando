54dcdee7c15f88a35cd4dc10fdc337e6b02df3c8
João Paulo Poffo <joao.poffo@hbsis.com.br>
2022-07-12 04:00:30 -0300

    icons + wakelock

--- app_v1/lib/main.dart
+++ app_v1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wakelock/wakelock.dart';
 
 import 'recording/record_to_stream.dart';
 import 'contacting/call.dart';
@@ -34,6 +35,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Prevent screen from going into sleep mode:
+    Wakelock.enable();
+  }
+
   @override
   Widget build(BuildContext context) {
     PredictionWidget predictor = PredictionWidget();
